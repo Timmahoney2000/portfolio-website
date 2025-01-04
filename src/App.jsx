9fcc5f4004d343fa8c95415264b85e881de7674b
Tim M <timmahoney2000@gmail.com>
2025-01-04 13:09:41 -0500

    added About page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div className="text-red-300">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
